66eedded98b2ade8c75f36ccb2161a67a505212b
Shikhar Tuli <shikhartuli98@gmail.com>
2020-12-29 20:03:26 +0530

    Added job script

--- CNN Design Space/nasbench_multi.sh
+++ CNN Design Space/nasbench_multi.sh
@@ -1,69 +1,71 @@
 #!/bin/bash
 
+# Export all variables to environment
+set -a
+
 numNodes=1
 numTasks=4
 cluster="adroit"
-gpu="tesla_v100"
-
-# while test $# -gt 0; do
-# case "$1" in
-#     -numNodes)
-#         shift
-#         $numNodes=$1
-#         shift
-#         ;;
-#     -numTasks)
-#         shift
-#         $numTasks=$1
-#         shift
-#         ;;
-#     -cluster)
-#         shift
-#         $cluster=$1
-#         shift
-#         ;;
-#     *)
-#        echo "Unrecognized flag -$1"
-#        return 1;
-#        ;;
-# esac
-# done  
-
-# if [[ $cluster -eq "adroit" ]]
-# then
-# 	$gpu="gpu:tesla_v100:1"
-# elif [[ $cluster -eq "tiger" ]]
-# then
-# 	$gpu="gpu:1"
-# else
-# 	echo "Unrecognized cluster"
-# 	return 1
-# fi
-
-#SBATCH --job-name=nasbench_multi           # create a short name for your job
-#SBATCH --nodes=1                   # node count
-#SBATCH --ntasks=4                  # total number of tasks across all nodes
-#SBATCH --cpus-per-task=4                   # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=8G                    # memory per cpu-core (4G is default)
-#SBATCH --gres=gpu:tesla_v100:1                 # number of gpus per node
-#SBATCH --time=00:50:00                     # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=begin                   # send email when job begins
-#SBATCH --mail-type=end                     # send email when job ends
-#SBATCH --mail-user=stuli@princeton.edu
-
-module purge
-module load anaconda3/2020.7
-conda activate acc-nn
-
+cluster_gpu="gpu:tesla_v100:1"
 numVertices=2
 
-python generate_graphs_script.py --max_vertices $numVertices
+Help()
+{
+   # Display Help
+   echo "Flags for this script"
+   echo
+   echo "Syntax: scriptTemplate [-numNodes|numTasks|cluster|vertices]"
+   echo "options:"
+   echo "-numNodes [default = 1]				Number of nodes to use in cluster"
+   echo "-numTasks [default = 4]				Number of tasks across all nodes"
+   echo "-cluster [default = \"adroit\"]		Selected cluster - adroit or tiger"
+   echo "-vertices [default = 2]				Number of vertices per module in NASBench"
+   echo "-help									Call this help message"
+   echo
+}
 
-for i in {0..($numTasks-1)}
-do
-	srun -N 1 -n 1 python run_evaluation_script.py --worker_id i --total_workers $numTasks --module_vertices $numVertices &
-done
+while test $# -gt 0; do
+case "$1" in
+    -numNodes)
+        shift
+        $numNodes=$1
+        shift
+        ;;
+    -numTasks)
+        shift
+        $numTasks=$1
+        shift
+        ;;
+    -cluster)
+        shift
+        $cluster=$1
+        shift
+        ;;
+    -vertices)
+        shift
+        $cluster=$1
+        shift
+        ;;    
+    -help)
+       Help
+       return 1;
+       ;;
+    *)
+       echo "Unrecognized flag -$1"
+       return 1;
+       ;;
+esac
+done  
 
-wait
+if [[ $cluster -eq "adroit" ]]
+then
+	cluster_gpu="gpu:tesla_v100:1"
+elif [[ $cluster -eq "tiger" ]]
+then
+	cluster_gpu="gpu:1"
+else
+	echo "Unrecognized cluster"
+	return 1
+fi
 
-python cleanup_script.py
+sbatch job_nasbench.slurm
