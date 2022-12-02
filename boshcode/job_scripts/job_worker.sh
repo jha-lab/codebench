8998de74905cf12d0b03448132b9f58e1d246fb4
shikhartuli <shikhartuli98@gmail.com>
2022-12-02 12:05:54 -0500

    Updated job scripts

--- boshcode/job_scripts/job_worker.sh
+++ boshcode/job_scripts/job_worker.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Script to train a given model
-
 # Author : Shikhar Tuli
 
-cluster="tiger"
-id="stuli"
 autotune="0"
 train_cnn="0"
 cnn_model_hash=""
@@ -31,9 +28,6 @@ Help()
    echo
    echo -e "Syntax: source ${CYAN}./job_scripts/job_train.sh${ENDC} [${YELLOW}flags${ENDC}]"
    echo "Flags:"
-   echo -e "${YELLOW}-c${ENDC} | ${YELLOW}--cluster${ENDC} [default = ${GREEN}\"tiger\"${ENDC}] \t\t Selected cluster - adroit, tiger or della"
-   echo -e "${YELLOW}-p${ENDC} | ${YELLOW}--partition${ENDC} [default = ${GREEN}\"gpu\"${ENDC}] \t\t Selected partition if cluster is della"
-   echo -e "${YELLOW}-i${ENDC} | ${YELLOW}--id${ENDC} [default = ${GREEN}\"stuli\"${ENDC}] \t\t\t Selected PU-NetID to email slurm updates"
    echo -e "${YELLOW}-a${ENDC} | ${YELLOW}--autotune${ENDC} [default = ${GREEN}\"0\"${ENDC}] \t\t To autotune the given CNN model"
    echo -e "${YELLOW}-t${ENDC} | ${YELLOW}--train_cnn${ENDC} [default = ${GREEN}\"0\"${ENDC}] \t\t To train the given CNN model"
    echo -e "${YELLOW}-m${ENDC} | ${YELLOW}--cnn_model_hash${ENDC} [default = ${GREEN}\"\"${ENDC}] \t\t CNN Model hash"
@@ -52,21 +46,6 @@ Help()
 while [[ $# -gt 0 ]]
 do
 case "$1" in
-    -c | --cluster)
-        shift
-        cluster=$1
-        shift
-        ;;
-    -p | --partition)
-        shift
-        partition=$1
-        shift
-        ;;
-    -i | --id)
-        shift
-        id=$1
-        shift
-        ;;
     -a | --autotune)
         shift
         autotune=$1
@@ -133,20 +112,6 @@ case "$1" in
 esac
 done  
 
-if [[ $cluster == "adroit" ]]
-then
-  cluster_gpu="gpu:tesla_v100:4"
-elif [[ $cluster == "tiger" ]]
-then
-  cluster_gpu="gpu:4"
-elif [[ $cluster == "della" ]]
-then
-  cluster_gpu="gpu:2"
-else
-    echo "Unrecognized cluster"
-    return 1
-fi
-
 job_file="./job_${accel_hash}.slurm"
 mkdir -p "./job_scripts/${dataset}/"
 
@@ -155,29 +120,16 @@ cd "./job_scripts/${dataset}/"
 # Create SLURM job script to train CNN-Accelerator pair
 echo "#!/bin/bash" >> $job_file
 echo "#SBATCH --job-name=code_${dataset}_${accel_hash}       # create a short name for your job" >> $job_file
-if [[ $partition == "gpu-ee" ]]
-then
-    echo "#SBATCH --partition gpu-ee                         # partition" >> $job_file
-fi
 echo "#SBATCH --nodes=1                                      # node count" >> $job_file
 echo "#SBATCH --ntasks=1                                     # total number of tasks across all nodes" >> $job_file
 echo "#SBATCH --cpus-per-task=24                             # cpu-cores per task (>1 if multi-threaded tasks)" >> $job_file
-# echo "#SBATCH --cpus-per-task=4                              # cpu-cores per task (>1 if multi-threaded tasks)" >> $job_file
 echo "#SBATCH --mem=128G                                     # memory per cpu-core (4G is default)" >> $job_file
-# echo "#SBATCH --mem=32G                                      # memory per cpu-core (4G is default)" >> $job_file
+echo "#SBATCH --gres=gpu:1" >> $job_file
 if [[ $train_cnn == "1" ]]
-then
-    echo "#SBATCH --gres=${cluster_gpu}                      # number of gpus per node" >> $job_file
-    # echo "#SBATCH --gres=gpu:1" >> $job_file
-fi
 echo "#SBATCH --time=144:00:00                               # total run time limit (HH:MM:SS)" >> $job_file
-# echo "#SBATCH --time=36:00:00                                # total run time limit (HH:MM:SS)" >> $job_file
-# echo "#SBATCH --mail-type=all                                # send email" >> $job_file
-# echo "#SBATCH --mail-user=stuli@princeton.edu" >> $job_file
 echo "" >> $job_file
 echo "module purge" >> $job_file
 echo "module load anaconda3/2020.7" >> $job_file
-# echo "conda init bash" >> $job_file
 echo "conda activate cnnbench" >> $job_file
 echo "" >> $job_file
 echo "cd ../.." >> $job_file
